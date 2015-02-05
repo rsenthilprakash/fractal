f38a639755f279cb877d3f980ec955db582f9fa3
senthil <senthil@sramali2-w530.org>
2015-02-05 13:45:50 +0530

    more ifs shapes

--- ifs.h
+++ ifs.h
@@ -34,7 +34,11 @@ struct shapeData {
 
 static const size_t INVALID_SHAPE = 10000;
 
-/* Add new shapes here */
+/* Add new shapes here
+ * some ref:
+ * http://www.susqu.edu/brakke/aux/ifs/gallery/gallery.htm
+ * http://classes.yale.edu/fractals/IntroToFrac/InvProb/InvProbNatFrac.html
+ */
 
 /* a kind of spiral */
 struct affineTrans rules_spiral_1[] = { {0.8188251, 0.8711631, -0.1568270, 0.9451363, 0.5044009, 0.2600615, 0.5000000, 0},
@@ -47,6 +51,23 @@ struct affineTrans rules_spiral_2[] = { {0.79874, -0.29072, 0.29072, 0.79874, 0.
 struct dimenstion dim_spiral_2 = {2, 2, 1, 1, 1, 1};
 
 
+/* Queen Anne's Lace */
+struct affineTrans rules_lace[] = { {0.27, 0.0, 0.0, 0.27, 1.0, 0.0, 0.08332, 0},
+                                    {0.27, 0.0, 0.0, 0.27, 0.707, 0.707, 0.08332, 0},
+                                    {0.27, 0.0, 0.0, 0.27, 0.0, 1.0, 0.08332, 0},
+                                    {0.27, 0.0, 0.0, 0.27, -0.707, 0.707, 0.08332, 0},
+                                    {0.27, 0.0, 0.0, 0.27, -1.0, 0.0, 0.08332, 0},
+                                    {0.27, 0.0, 0.0, 0.27, -0.707, -0.707, 0.08332, 0},
+                                    {0.27, 0.0, 0.0, 0.27, 0.0, -1.0, 0.08332, 0},
+                                    {0.27, 0.0, 0.0, 0.27, 0.707, -0.707, 0.08332, 0},
+                                    {0.46194, -0.19134, 0.19134, 0.46194, 0.0, 0.0, 0.30005, 0} };
+struct dimenstion dim_lace = {4, 4, 1, 1, 2, 2};
+
+/* star_1 */
+struct affineTrans rules_star_1[] = { {0.4847246,  0.5698310, -0.1388086,  0.7466392, -0.2309255,  1.6087142,  0.5000000, 0},
+                                      {0.7415562,  1.0137806, -0.2713263,  0.8852760, -1.8673942, -1.8310378,  0.5000000, 0} };
+struct dimenstion dim_star_1 = {40, 40, 1, 1, 20, 20};
+
 /* snowflake */
 struct affineTrans rules_snowflake[] = { {0.6, 0.0, 0.0, 0.6, 0.0, 0.0, 0.25, 0},
                                          {0.4, 0.0, 0.0, 0.2, 0.6, 0.0, 0.06, 0},
@@ -56,6 +77,8 @@ struct dimenstion dim_snowflake = {2, 2, 1, 1, 1, 1};
 
 struct shapeData shape_database[] = { {"spiral_1", rules_spiral_1, 2, &dim_spiral_1},
                                       {"spiral_2", rules_spiral_2, 2, &dim_spiral_2},
+                                      {"lace", rules_lace, 9, &dim_lace},
+                                      {"star_1", rules_star_1, 2, &dim_star_1},
                                       {"snowflake", rules_snowflake, 3, &dim_snowflake} };
 
 #endif
