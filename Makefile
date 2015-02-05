f14b1ace73f0aa7e9e6caa43280437162eab7d06
senthil <senthil@sramali2-w530.org>
2015-02-05 11:50:39 +0530

    Proper .h dependency in Makefile

--- Makefile
+++ Makefile
@@ -4,26 +4,45 @@ LIBS = -lm
 PNG_CFLAGS = `pkg-config --cflags libpng`
 PNG_LIBS = `pkg-config --libs libpng`
 
+SOURCES = fern.c \
+          png_utils.c \
+          poly.c \
+          tree.c \
+          maple.c \
+          ifs.c
+
 TARGETS =  fern poly tree maple ifs
 
 .PHONY: all
 all: $(TARGETS)
 
-fern: fern.c png_utils.c
-	$(CC) $(CFLAGS) $(PNG_CFLAGS) $^ $(PNG_LIBS) $(LIBS) -o $@
+fern: fern.o png_utils.o
+	$(CC) $^ $(PNG_LIBS) $(LIBS) -o $@
 
-poly: poly.c png_utils.c
-	$(CC) $(CFLAGS) $(PNG_CFLAGS) $^ $(PNG_LIBS) $(LIBS) -o $@
+poly: poly.o png_utils.o
+	$(CC) $^ $(PNG_LIBS) $(LIBS) -o $@
 
-tree: tree.c png_utils.c
-	$(CC) $(CFLAGS) $(PNG_CFLAGS) $^ $(PNG_LIBS) $(LIBS) -o $@
+tree: tree.o png_utils.o
+	$(CC) $^ $(PNG_LIBS) $(LIBS) -o $@
 
-maple: maple.c png_utils.c
-	$(CC) $(CFLAGS) $(PNG_CFLAGS) $^ $(PNG_LIBS) $(LIBS) -o $@
+maple: maple.o png_utils.o
+	$(CC) $^ $(PNG_LIBS) $(LIBS) -o $@
 
-ifs: ifs.c png_utils.c
-	$(CC) $(CFLAGS) $(PNG_CFLAGS) $^ $(PNG_LIBS) $(LIBS) -o $@
+ifs: ifs.o png_utils.o
+	$(CC) $^ $(PNG_LIBS) $(LIBS) -o $@
+
+.c.o:
+	$(CC) -c $(CFLAGS) $(PNG_CFLAGS) $< -o $@
+depend: .depend
+
+.depend: $(SOURCES)
+	rm -f ./.depend
+	$(CC) $(CFLAGS) $(PNG_CFLAGS) -MM $^ > ./.depend;
+
+include .depend
 
 .PHONY: clean
 clean:
 	rm -rf $(TARGETS)
+	rm -rf *.o
+	rm -rf ./.depend
--- ifs.h
+++ ifs.h
@@ -37,9 +37,14 @@ static const size_t INVALID_SHAPE = 10000;
 /* Add new shapes here */
 
 /* a kind of spiral */
-struct affineTrans rules_spiral[] = { {0.8188251, 0.8711631, -0.1568270, 0.9451363, 0.5044009, 0.2600615, 0.5000000, 0},
-                                      {0.3899677, -0.6386825, -0.2169008, -0.1310590, -0.4387605, 0.6664247, 0.5000000, 0} };
-struct dimenstion dim_spiral = {10, 10, 1, 2, 2, 4};
+struct affineTrans rules_spiral_1[] = { {0.8188251, 0.8711631, -0.1568270, 0.9451363, 0.5044009, 0.2600615, 0.5000000, 0},
+                                        {0.3899677, -0.6386825, -0.2169008, -0.1310590, -0.4387605, 0.6664247, 0.5000000, 0} };
+struct dimenstion dim_spiral_1 = {10, 10, 1, 2, 2, 4};
+
+/* another kind of spiral */
+struct affineTrans rules_spiral_2[] = { {0.79874, -0.29072, 0.29072, 0.79874, 0.0, 0.0, 0.88923, 0},
+                                        {0.3, 0.0, 0.0, 0.3, 0.7, 0.0, 0.11077, 0} };
+struct dimenstion dim_spiral_2 = {2, 2, 1, 1, 1, 1};
 
 
 /* snowflake */
@@ -49,7 +54,8 @@ struct affineTrans rules_snowflake[] = { {0.6, 0.0, 0.0, 0.6, 0.0, 0.0, 0.25, 0}
 struct dimenstion dim_snowflake = {2, 2, 1, 1, 1, 1};
 
 
-struct shapeData shape_database[] = { {"spiral", rules_spiral, 2, &dim_spiral},
+struct shapeData shape_database[] = { {"spiral_1", rules_spiral_1, 2, &dim_spiral_1},
+                                      {"spiral_2", rules_spiral_2, 2, &dim_spiral_2},
                                       {"snowflake", rules_snowflake, 3, &dim_snowflake} };
 
 #endif
